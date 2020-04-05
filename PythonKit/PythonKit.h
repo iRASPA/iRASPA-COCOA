60442d009aabbf8f0c5e6090e308740c81a8e62f
David Dubbeldam <d.dubbeldam@uva.nl>
2020-04-05 17:47:54 +0200

    • Added 'PythonKit'.

---
+++ PythonKit/PythonKit.h
@@ -0,0 +1,19 @@
+//
+//  PythonKit.h
+//  PythonKit
+//
+//  Created by David Dubbeldam on 05/04/2020.
+//  Copyright © 2020 David Dubbeldam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PythonKit.
+FOUNDATION_EXPORT double PythonKitVersionNumber;
+
+//! Project version string for PythonKit.
+FOUNDATION_EXPORT const unsigned char PythonKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PythonKit/PublicHeader.h>
+
+
