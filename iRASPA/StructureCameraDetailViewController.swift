bc6004dbbcc7874efb9f3ea979027e378ec19322
David Dubbeldam <d.dubbeldam@uva.nl>
2021-04-30 09:43:09 +0200

    • Fixed tags in Euler-angles in the camera detailView.

--- iRASPA/StructureCameraDetailViewController.swift
+++ iRASPA/StructureCameraDetailViewController.swift
@@ -334,12 +334,12 @@ class StructureCameraDetailViewController: NSViewController, NSOutlineViewDelega
         }
       }
       
-      if let sliderEulerAngleX: NSSlider = view.viewWithTag(8) as? NSSlider,
-         let sliderEulerAngleZ: NSSlider = view.viewWithTag(9) as? NSSlider,
-         let sliderEulerAngleY: NSSlider = view.viewWithTag(10) as? NSSlider,
-         let textFieldEulerAngleX: NSTextField = view.viewWithTag(11) as? NSTextField,
-         let textFieldEulerAngleZ: NSTextField = view.viewWithTag(12) as? NSTextField,
-         let textFieldEulerAngleY: NSTextField = view.viewWithTag(13) as? NSTextField
+      if let textFieldEulerAngleX: NSTextField = view.viewWithTag(8) as? NSTextField,
+         let textFieldEulerAngleZ: NSTextField = view.viewWithTag(9) as? NSTextField,
+         let textFieldEulerAngleY: NSTextField = view.viewWithTag(10) as? NSTextField,
+         let sliderEulerAngleX: NSSlider = view.viewWithTag(11) as? NSSlider,
+         let sliderEulerAngleZ: NSSlider = view.viewWithTag(12) as? NSSlider,
+         let sliderEulerAngleY: NSSlider = view.viewWithTag(13) as? NSSlider
       {
         textFieldEulerAngleX.isEnabled = false
         sliderEulerAngleX.isEnabled = false
